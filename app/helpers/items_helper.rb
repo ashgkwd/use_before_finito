1d5c53b5a3874e71c28149fb06cc542b16576f3c
Ashish Gaikwad <ash.gkwd@gmail.com>
2024-04-19 23:41:45 -0400

    feat: removes created_at from table, shows days till 9

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,26 +1,35 @@
 module ItemsHelper
+  def sweet_date(date)
+    if date.year == Date.today.year
+      date.strftime("%d %b")
+    else
+      date.strftime("%b %Y")
+    end
+  end
+
   def color_of(date)
     case days_difference(date)
     when -Float::INFINITY...1
+      "badge text-bg-dark"
+    when 1..2
       "badge text-bg-danger"
-    when 1..3
+    when 3..5
       "badge text-bg-warning"
-    when 4..9
-      "badge text-bg-info"
+    when 6..9
+      "badge text-bg-success"
     when 9..Float::INFINITY
       "badge text-bg-light"
     end
   end
 
   def badge_of(date)
-    case days_difference(date)
+    remaining = days_difference(date)
+    case remaining
     when -Float::INFINITY...1
       "EXPIRED"
-    when 1..3
-      "IN 3 DAYS"
-    when 4..9
-      "IN A WEEK"
-    when 9..Float::INFINITY
+    when 1..9
+      "IN #{remaining} DAYS"
+    when 10..Float::INFINITY
       "FUTURE"
     end
   end
